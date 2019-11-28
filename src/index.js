205abbfb7eb7bd134941b9225dc6c598350925e4
Hovo Petrosyan <hovpetrosyan.github@gmail.com>
2019-11-29 00:24:44 +0400

    setup: webpack,babel,eslint,prettier configuration is done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Test from "./screens/Test";
+
+ReactDOM.render(<Test />, document.getElementById("container"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: { extensions: [".js", ".jsx"] }
+};
